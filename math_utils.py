655aeae66f9571a14d90a87c435625903746b2c0
mohammednuru2026 <mohammednuru2026@gmail.com>
2026-01-31 03:36:21 -0800

    Add files via upload

---
+++ math_utils.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Aug 14 11:40:37 2025
+
+@author: admin
+"""
+
+# math_utils.py
+
+def add(x, y):
+    return x + y
+
+def multiply(x, y):
+    return x * y
